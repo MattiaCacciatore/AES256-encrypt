d82c8673c881651cab76e11553f42ab7ae6252fb
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-26 11:15:23 +0100

    Small adjustment.

--- AES256_encrypted.h
+++ AES256_encrypted.h
@@ -22,38 +22,37 @@
 /*------------------------------------------------------------------------------------------------------------------------------*/
 //  MACROS
 /*------------------------------------------------------------------------------------------------------------------------------*/
-#define BOX_SIZE      16  /*  Size of s_box. */
-#define STATE_SIZE    4   /*  Size of state matrix. */
-#define KEY_SIZE      32  /*  Size of SecretKey in bytes. 16 --> 128, 24 --> 192, 32 --> 256. */
-#define TEXT_SIZE     16  /*  Size of text/message/password. */
-#define TABLE_SIZE    256 /*  Size of tables in GF(2^8) implemented as array. */
-#define NUM_ROUNDS    14  /*  Number of encrypt round repetitions. 10 --> 128, 12 --> 192, 14 --> 256. */
-#define NUM_BLOCKS    4   /*  Number of words/columns in block input like state matrix. */
-#define NUM_KEY       8   /*  Number of words/columns in the key. 4 --> 128, 6 --> 192, 8 --> 256. */
-
-namespace aes256{
+#define BOX_SIZE      16  // Size of s_box.
+#define STATE_SIZE    4   // Size of state matrix.
+#define KEY_SIZE      32  // Size of SecretKey in bytes. 16 --> 128, 24 --> 192, 32 --> 256.
+#define TEXT_SIZE     16  // Size of text/message/password.
+#define TABLE_SIZE    256 // Size of tables in GF(2^8) implemented as array.
+#define NUM_ROUNDS    14  // Number of encrypt round repetitions. 10 --> 128, 12 --> 192, 14 --> 256.
+#define NUM_BLOCKS    4   // Number of words/columns in block input like state matrix.
+#define NUM_KEY       8   // Number of words/columns in the key. 4 --> 128, 6 --> 192, 8 --> 256.
 /*------------------------------------------------------------------------------------------------------------------------------*/
 //	FUNCTIONS
 /*------------------------------------------------------------------------------------------------------------------------------*/
-/*	Encrypt key using AES-256 algorithm. First array input, second matrix output. */
+namespace aes256{
+// Encrypt key using AES-256 algorithm. First array input, second matrix output.
 void Encrypt(const unsigned char [TEXT_SIZE], unsigned char [NUM_BLOCKS][NUM_BLOCKS]);
-/*	It creates other Nr keys based on the first key (first key = plaintext). */
+// It creates other Nr keys based on the first key (first key = plaintext).
 void KeyExpansion(unsigned char [NUM_BLOCKS][NUM_BLOCKS * (NUM_ROUNDS + 1)]);
-/*	Rijndael key schedule. */
+// Rijndael key schedule.
 void AESKeyScheduler(const unsigned char [NUM_BLOCKS][NUM_BLOCKS * (NUM_ROUNDS + 1)], unsigned char [NUM_BLOCKS][NUM_BLOCKS], const int);
-/*	First step of AES-256 algorithm. It substitute matrix values with Rijndael's s_box values. */
+// First step of AES-256 algorithm. It substitute matrix values with Rijndael's s_box values.
 void SubBytes(unsigned char [NUM_BLOCKS][NUM_BLOCKS]);
-/*  Second step of AES-256 algorithm. It switchs rows between them creating confusion. */
+// Second step of AES-256 algorithm. It switchs rows between them creating confusion.
 void ShiftRows(unsigned char [NUM_BLOCKS][NUM_BLOCKS]);
-/*  Third step of AES-256 algorithm. It diffuses confusion. */
+// Third step of AES-256 algorithm. It diffuses confusion.
 void MixColumns(unsigned char [NUM_BLOCKS][NUM_BLOCKS]);
-/*  Fourth and last step of AES-256 algorithm. It permutes state matrix with roundkey using XOR operator. */
+// Fourth and last step of AES-256 algorithm. It permutes state matrix with roundkey using XOR operator.
 void AddRoundKey(unsigned char [NUM_BLOCKS][NUM_BLOCKS], const unsigned char [NUM_BLOCKS][NUM_BLOCKS]);
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
 //	OTHER FUNCTIONS
 /*------------------------------------------------------------------------------------------------------------------------------*/
-/*  Print matrix 4 x 4 */
+// Print matrix 4 x 4.
 void PrintMatrix(const unsigned char [NUM_BLOCKS][NUM_BLOCKS]);
-/*  Print matrix 4 x 68 */
+// Print matrix 4 x 68.
 void PrintExpandedMatrix(const unsigned char [NUM_BLOCKS][NUM_BLOCKS * (NUM_ROUNDS + 1)]);
